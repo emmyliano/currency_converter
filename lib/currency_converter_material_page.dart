92360b08927bd4b69854938302a5c4988d7fbb97
Akemu Emmanuel <akemuemmy@gmail.com>
2024-03-21 15:21:24 +0100

    Solved the bug. First flutter project done

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -12,15 +12,37 @@ class _CurrencyConverterMaterialPageState
     extends State<CurrencyConverterMaterialPage> {
   double result = 0;
 
-  final TextEditingController textEditingController = TextEditingController();
-  // void convert() {
-  //   result = double.parse(textEditingController.text) * 80;
-  //   setState(() {});
-  // }
+  final TextEditingController amountController = TextEditingController();
+  // ignore: non_constant_identifier_names
+  double dollar_to_naira_rate_now = 1413.0172;
+  String lastUpdate = 'Last Update: 2024-3-21 15:07';
+
+  void convert() {
+    setState(() {
+      String input = amountController.text.trim();
+      if (input.isNotEmpty && double.tryParse(input) != null) {
+        result = double.parse(input) * dollar_to_naira_rate_now;
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+                'Dollar to Naira Exchange rate at $lastUpdate is $dollar_to_naira_rate_now'),
+          ),
+        );
+      } else {
+        result = 0;
+        // display an error message
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Please enter a valid number'),
+          ),
+        );
+      }
+    });
+  }
 
   @override
   void dispose() {
-    textEditingController.dispose();
+    amountController.dispose();
     super.dispose();
   }
 
@@ -35,47 +57,47 @@ class _CurrencyConverterMaterialPageState
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                'INR ${result !=0 ? result.toStringAsFixed(3) : result.toStringAsFixed(0)}',
+                'NGN ${result != 0 ? result.toStringAsFixed(3) : result.toStringAsFixed(0)}',
+                // result.toString(),
                 style: const TextStyle(
                   fontSize: 55,
                   fontWeight: FontWeight.bold,
                   color: Color.fromARGB(255, 255, 255, 255),
                 ),
               ),
-
               Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  style: const TextStyle(
-                    color: Colors.black,
-                  ),
-                  decoration: InputDecoration(
-                    hintText: 'Please enter the amount in USD',
-                    hintStyle: const TextStyle(
+                padding: const EdgeInsets.all(8.2),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: TextField(
+                    controller: amountController,
+                    style: const TextStyle(
                       color: Colors.black,
                     ),
-                    prefixIcon: const Icon(Icons.monetization_on_outlined),
-                    prefixIconColor: Colors.black,
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                        width: 2.0,
-                        style: BorderStyle.solid,
+                    decoration: InputDecoration(
+                      hintText: 'Please enter the amount in USD',
+                      hintStyle: const TextStyle(
+                        color: Colors.black,
                       ),
-                      borderRadius: BorderRadius.circular(60),
+                      prefixIcon: const Icon(Icons.monetization_on_outlined),
+                      prefixIconColor: Colors.black,
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          width: 2.0,
+                          style: BorderStyle.solid,
+                        ),
+                        borderRadius: BorderRadius.circular(60),
+                      ),
                     ),
                   ),
                 ),
               ),
-
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextButton(
-                  onPressed: () {
-                    result = double.parse(textEditingController.text) * 81;
-                    setState(() {});
-                  },
+                  onPressed: convert,
                   style: ButtonStyle(
                       backgroundColor:
                           const MaterialStatePropertyAll(Colors.black),
